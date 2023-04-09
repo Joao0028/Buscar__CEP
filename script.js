af46b59180039f4ad68d1b308b385708df2494d2
João Paulo Nascimento Silva <120075584+Joao0028@users.noreply.github.com>
2023-04-09 20:18:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+
+
+
+// O ".then()" serve para dizer que algo aconteça quando o parâmetro for executado corretamente.
+// O "catch" serve para dizer que algo aconteça caso o parâmetro passado for errado
